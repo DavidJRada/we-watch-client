579d8a0873c9249ea082789a1708dc159ea917b9
davidjrada <davidjrada@gmail.com>
2019-06-03 11:36:47 -0400

    getting ready for heroku

--- src/App.js
+++ src/App.js
@@ -11,6 +11,12 @@ import Footer from './components/Footer'
 import LoginPage from './components/LoginPage'
 
 
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:3000'
+} else {
+  baseURL = 'https://gitpub-backend.herokuapp.com'
+}
+
 class App extends Component {
   constructor(props) {
     super(props)
